b840ba13dd25a12683c43ab675184db8f8e489e8
yeftakun <yeftakun34@gmail.com>
2025-05-19 07:17:57 +0800

    add auth for other pages

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -9,4 +9,26 @@ function homeAuth(req, res, next) {
   return res.status(403).send('Akses ditolak');
 }
 
-module.exports = { homeAuth };
+function sessionAuth(req, res, next) {
+  if (!req.session || !req.session.loggedin) {
+    return res.redirect('/'); // redirect ke login
+  }
+  const role = req.session.role;
+  if (role === 'admin' || role === 'teacher') {
+    return next();
+  }
+  return res.status(403).render('403');
+}
+
+function dataAuth(req, res, next) {
+  if (!req.session || !req.session.loggedin) {
+    return res.redirect('/'); // redirect ke login
+  }
+  const role = req.session.role;
+  if (role === 'admin') {
+    return next();
+  }
+  return res.status(403).render('403');
+}
+
+module.exports = { homeAuth, sessionAuth, dataAuth };
